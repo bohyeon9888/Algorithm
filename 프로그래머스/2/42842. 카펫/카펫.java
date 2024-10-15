f6bb1450b2c75d80f51969a6e9b1fb141a24ec46
BOHYEON <49276159+bohyeon9888@users.noreply.github.com>
2024-10-15 11:30:13 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int sum = brown + yellow;
+        
+        for (int i = 3; i <= sum; i++) {
+            int j = sum / i;
+            
+            if (sum % i == 0 && j >= 3) {
+                int w = Math.max(i, j);
+                int h = Math.min(i, j);
+                
+                int cnt = (w - 2) * (h - 2);
+                if (cnt == yellow) {
+                    answer[0] = w;
+                    answer[1] = h;
+                    return answer;
+                } 
+            }
+            
+        }
+        
+        return answer;
+    }
+}
